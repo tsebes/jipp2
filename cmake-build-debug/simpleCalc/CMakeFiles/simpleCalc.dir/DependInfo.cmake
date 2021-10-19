2709be0f3ffd2c8e2abb3eee0535d43a1e46b4be
tsebes <sebastian.zych@student.pk.edu.pl>
2021-10-19 09:21:01 +0200

    1

---
+++ cmake-build-debug/simpleCalc/CMakeFiles/simpleCalc.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/sebiz/CLionProjects/jipp2/simpleCalc/src/calc.cpp" "C:/Users/sebiz/CLionProjects/jipp2/cmake-build-debug/simpleCalc/CMakeFiles/simpleCalc.dir/src/calc.cpp.obj"
+  "C:/Users/sebiz/CLionProjects/jipp2/simpleCalc/src/main.cpp" "C:/Users/sebiz/CLionProjects/jipp2/cmake-build-debug/simpleCalc/CMakeFiles/simpleCalc.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../simpleCalc/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/simpleCalc/CMakeFiles/simpleCalc.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpleCalc.dir/src/calc.cpp.obj"
+  "CMakeFiles/simpleCalc.dir/src/main.cpp.obj"
+  "simpleCalc.exe"
+  "simpleCalc.exe.manifest"
+  "simpleCalc.lib"
+  "simpleCalc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simpleCalc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
